e61d0e084faa75dcc60293acf21f662e687c42d6
borisey <slamsnv@gmail.com>
2024-10-15 21:01:05 +0300

    horse canMoveToPosition

--- src/main/java/org/chess/chess_pieces/Horse.java
+++ src/main/java/org/chess/chess_pieces/Horse.java
@@ -9,7 +9,25 @@ public class Horse extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        return false;
+        // Конь не может выйти за пределы доски
+        if (toLine > 7 || toLine < 0 || toColumn < 0 || toColumn > 7) return false;
+
+        // Конь может перемещаться вертикально
+        if (Math.abs(toLine - line) > 2 || Math.abs(toLine - line) < 1) return false;
+
+        // Конь может перемещаться горизонтально
+        if (Math.abs(toColumn - column) > 2 || (Math.abs(toColumn - column) < 1)) return false;
+
+        // Конь не может переместиться в ту же клетку
+        if ((line == toLine) && (column == toColumn)) return false;
+
+        // Конь должен сменить линию и колонку
+        if (Math.abs(line - toLine) <= 0 && Math.abs(toColumn - column) <= 0) return false;
+
+        // Конь не может двигаться по диагонали
+        if (Math.abs(line - toLine) == Math.abs(toColumn - column)) return false;
+
+        return true;
     }
 
     @Override
