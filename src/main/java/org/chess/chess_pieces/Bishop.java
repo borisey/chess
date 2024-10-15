3c751fe71c54584631e48b1f8c06501c24553623
borisey <slamsnv@gmail.com>
2024-10-15 21:09:12 +0300

    bishop canMoveToPosition

--- src/main/java/org/chess/chess_pieces/Bishop.java
+++ src/main/java/org/chess/chess_pieces/Bishop.java
@@ -9,7 +9,16 @@ public class Bishop extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        return false;
+        // Слон не может выйти за пределы доски
+        if (toLine > 7 || toLine < 0 || toColumn < 0 || toColumn > 7) return false;
+
+        // Слон может двигаться только по диагонали
+        if (Math.abs(toLine - line) != Math.abs(toColumn - column)) return false;
+
+        // Слон не может переместиться в ту же клетку
+        if ((line == toLine) && (column == toColumn)) return false;
+
+        return true;
     }
 
     @Override
