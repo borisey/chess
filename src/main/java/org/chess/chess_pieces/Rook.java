b6f041286f2ab4121df66978695c50ba9945a394
borisey <slamsnv@gmail.com>
2024-10-15 21:16:42 +0300

    rook canMoveToPosition

--- src/main/java/org/chess/chess_pieces/Rook.java
+++ src/main/java/org/chess/chess_pieces/Rook.java
@@ -9,7 +9,16 @@ public class Rook extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        return false;
+        // Ладья не может выйти за пределы доски
+        if (toLine > 7 || toLine < 0 || toColumn < 0 || toColumn > 7) return false;
+
+        // Ладья перемещаться только по прямой
+        if (Math.abs(toColumn-column) > 0 && Math.abs(toLine-line) > 0) return false;
+
+        // Ладья не может переместиться в ту же клетку
+        if ((line == toLine) && (column == toColumn)) return false;
+
+        return true;
     }
 
     @Override
