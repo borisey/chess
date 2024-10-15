1910d2a94a047fbcb169c545c5b3434cfa647878
borisey <slamsnv@gmail.com>
2024-10-15 16:05:39 +0300

    fix

--- src/main/java/org/chess/ChessBoard.java
+++ src/main/java/org/chess/ChessBoard.java
@@ -17,7 +17,7 @@ public class ChessBoard {
     public boolean moveToPosition(int startLine, int startColumn, int endLine, int endColumn) {
         if (checkPos(startLine) && checkPos(startColumn)) {
 
-            if (!nowPlayer.equals(board[startLine][startColumn].getColor())) return false;
+            if (!nowPlayer.equals(board[startLine][startColumn].getColor(""))) return false;
 
             if (board[startLine][startColumn].canMoveToPosition(this, startLine, startColumn, endLine, endColumn)) {
                 board[endLine][endColumn] = board[startLine][startColumn]; // if piece can move, we moved a piece
@@ -42,7 +42,7 @@ public class ChessBoard {
                 if (board[i][j] == null) {
                     System.out.print(".." + "\t");
                 } else {
-                    System.out.print(board[i][j].getSymbol() + board[i][j].getColor().substring(0, 1).toLowerCase() + "\t");
+                    System.out.print(board[i][j].getSymbol() + board[i][j].getColor("").substring(0, 1).toLowerCase() + "\t");
                 }
             }
             System.out.println();
@@ -54,4 +54,12 @@ public class ChessBoard {
     public boolean checkPos(int pos) {
         return pos >= 0 && pos <= 7;
     }
+
+    public boolean castling0() {
+        return true;
+    };
+
+    public boolean castling7() {
+        return true;
+    };
 }
