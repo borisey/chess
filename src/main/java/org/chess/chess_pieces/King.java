03fd7b547775578feafe78bf3e993e0bd357618a
borisey <slamsnv@gmail.com>
2024-10-15 21:29:47 +0300

    king canMoveToPosition

--- src/main/java/org/chess/chess_pieces/King.java
+++ src/main/java/org/chess/chess_pieces/King.java
@@ -9,7 +9,16 @@ public class King extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        return false;
+        // Король не может выйти за пределы доски
+        if (toLine > 7 || toLine < 0 || toColumn < 0 || toColumn > 7) return false;
+
+        // Король не может переместиться в ту же клетку
+        if ((line == toLine) && (column == toColumn)) return false;
+
+        // Король может переместиться в любое поле вокруг себя
+        if ((Math.abs(toLine - line) > 1) || (Math.abs(toColumn - column) > 1)) return false;
+
+        return true;
     }
 
     @Override
