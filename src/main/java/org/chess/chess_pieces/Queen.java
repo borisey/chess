056128a43150507dbed1b0f57c65c94af9ecca31
borisey <slamsnv@gmail.com>
2024-10-15 21:31:33 +0300

    fix Queen canMoveToPosition

--- src/main/java/org/chess/chess_pieces/Queen.java
+++ src/main/java/org/chess/chess_pieces/Queen.java
@@ -12,6 +12,15 @@ public class Queen extends ChessPiece {
         // Королева не может выйти за пределы доски
         if (toLine > 7 || toLine < 0 || toColumn < 0 || toColumn > 7) return false;
 
+        // Королева не может двигаться буквой Г
+        if (Math.abs(toLine - line) != (Math.abs(toColumn - column))) {
+            if (Math.abs(toLine - line) == 0 || Math.abs(toColumn - column) == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
         // Королева не может переместиться в ту же клетку
         if ((line == toLine) && (column == toColumn)) return false;
 
