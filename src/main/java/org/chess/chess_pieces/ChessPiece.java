8ecf1ccdf49c222475ce0eac83df1fc04e63717f
borisey <slamsnv@gmail.com>
2024-10-16 11:01:35 +0300

    fix

--- src/main/java/org/chess/chess_pieces/ChessPiece.java
+++ src/main/java/org/chess/chess_pieces/ChessPiece.java
@@ -1,6 +1,7 @@
 package org.chess.chess_pieces;
 
 import org.chess.ChessBoard;
+import java.util.ArrayList;
 
 public abstract class ChessPiece {
     public String color;
@@ -17,4 +18,35 @@ public abstract class ChessPiece {
     public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
 
     public abstract String getSymbol();
+
+    /**
+     * Метод проверяет, существует ли припятствие
+     */
+    public boolean isObstacleExist(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        ArrayList<ChessPiece> trajectory = new ArrayList<>();
+
+        if (line == toLine) {
+            for (int i = column + 1; i < toColumn; i++) {
+                trajectory.add(chessBoard.board[line][i]);
+            }
+        }
+
+        if (column == toColumn) {
+            for (int i = line + 1; i < toLine; i++) {
+                trajectory.add(chessBoard.board[i][column]);
+            }
+        }
+
+        if (Math.abs(column - toColumn) == Math.abs(line - toLine)) {
+            for (int i = line + 1, k = column + 1; i < toLine; i++, k++) {
+                trajectory.add(chessBoard.board[i][k]);
+            }
+        }
+
+        for (ChessPiece i: trajectory) {
+            if (i != null) return true;
+        }
+
+        return false;
+    }
 }
